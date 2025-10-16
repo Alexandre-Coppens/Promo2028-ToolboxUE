c6b6656e30ac8a7bc81328cb1c4399494b53974c
Alexandre Coppens <Alexandre.coppens@artfx.fr>
2025-10-16 08:43:44 +0200

    [Add] Dualsense Support Plugin

---
+++ Plugins/WindowsDualsense_ds5w/Source/WindowsDualsense_ds5w/WindowsDualsense_ds5w.Build.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2025 Rafael Valoto/Publisher. All rights reserved.
+// Created for: WindowsDualsense_ds5w - Plugin to support DualSense controller on Windows.
+// Planned Release Year: 2025
+
+
+using System.IO;
+using UnrealBuildTool;
+
+public class WindowsDualsense_ds5w : ModuleRules
+{
+	public WindowsDualsense_ds5w(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+ 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "ApplicationCore", "InputCore", "InputDevice",  "AudioMixer" });
+	    PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+	    bEnableExceptions = true;
+	    
+	    if (Target.Platform == UnrealTargetPlatform.Win64)
+	    {
+	        PublicAdditionalLibraries.Add("hid.lib");
+	    }
+	    
+	    if (Target.Platform == UnrealTargetPlatform.Linux || Target.Platform == UnrealTargetPlatform.Mac)
+	    {
+		    PrivateDependencyModuleNames.AddRange(new string[] { "SDL2" });
+	    }
+	}
+}
